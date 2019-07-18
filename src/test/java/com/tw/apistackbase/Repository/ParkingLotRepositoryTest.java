a8897bfa8418c93598e0d34d963c72f58cb6d1d4
retr0 <395637649@qq.com>
2019-07-18 19:41:42 +0800

    test:same name parking lot exception

---
+++ src/test/java/com/tw/apistackbase/Repository/ParkingLotRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.tw.apistackbase.Repository;
+
+import com.tw.apistackbase.Model.ParkingLot;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ParkingLotRepositoryTest {
+
+    @Autowired
+    private ParkingLotRepository parkingLotRepository;
+
+    private List<ParkingLot> parkingLots;
+
+    @Before
+    public void setUp() {
+        parkingLots = IntStream.rangeClosed(1, 2).boxed()
+                .map(x -> new ParkingLot(x + ": name", x, x + ": location"))
+                .collect(Collectors.toList());
+    }
+
+    @Test
+    public void should_error_when_save_same_name_parkingLot() {
+        // given
+        parkingLotRepository.save(parkingLots.get(0));
+        ParkingLot parkingLot = new ParkingLot(parkingLots.get(0).getName(),10,"GuangZhou");
+        // when
+        assertThrows(DataIntegrityViolationException.class,()->{
+            parkingLotRepository.save(parkingLot);
+            parkingLotRepository.flush();
+        });
+    }
+}
